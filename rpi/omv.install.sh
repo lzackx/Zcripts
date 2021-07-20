d1f60bc79d966c54971a31a27c943b6cf2c1baa8
lzackx <lzackx@lzackx.com>
2021-07-20 18:12:20 +0800

    add: omv.install.sh

---
+++ rpi/omv.install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Pull Resources
+sudo apt install git
+
+mkdir -p ~/Repositories
+cd ~/Repositories
+git clone https://github.com/lzackx/Zcripts.git
+
+if [ ! -d Zcripts ];then
+    exit 1;
+fi
+
+cd Zcripts
+sudo cp ./rpi/omv.hosts /etc/hosts
+
+cd ~/Repositories
+
+sudo apt update
+sudo apt upgrade -y
+
+# Installation
+sudo apt install vlc-bin -y
+
+sudo apt install vim -y
+cp ~/Repositories/Zcripts/zevn/.vimvc ~/
+
+# zsh
+sudo apt install zsh -y
+wget -O - https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sudo bash
+cp ~/Repositories/Zcripts/zevn/.zevn ~/
+
+# OpenMediaVault
+wget -O - https://github.com/OpenMediaVault-Plugin-Developers/installScript/raw/master/install | sudo bash
+
+#sudo omv-salt deploy run
